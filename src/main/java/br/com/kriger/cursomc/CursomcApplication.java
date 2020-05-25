a83b32836de821289114d6590cd85b77e4626362
Márcio Sá <sa.marcio@gmail.com>
2020-05-25 17:16:13 -0300

    Criação Classe Produtos e associação de Categoria N2N.

--- src/main/java/br/com/kriger/cursomc/CursomcApplication.java
+++ src/main/java/br/com/kriger/cursomc/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.kriger.cursomc.domain.Categoria;
+import br.com.kriger.cursomc.domain.Produto;
 import br.com.kriger.cursomc.repositories.CategoriaRepository;
+import br.com.kriger.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -25,7 +29,20 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria categoria1 = new Categoria("Informática");
 		Categoria categoria2 = new Categoria("Escritório");
 		
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
+
+		categoria1.setProdutos(Arrays.asList(produto1, produto2, produto3));
+		categoria2.setProdutos(Arrays.asList(produto2));
+
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));		
+
 	}
 
 }
---
+++ src/main/java/br/com/kriger/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package br.com.kriger.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.kriger.cursomc.domain.Produto;
+
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> { 
+
+}
