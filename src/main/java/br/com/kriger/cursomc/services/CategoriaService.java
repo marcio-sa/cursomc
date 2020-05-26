5996757d19828a42c978ee47e01833298ff99507
Márcio Sá <sa.marcio@gmail.com>
2020-05-25 23:00:25 -0300

    Tratamento de exceção e ajustes no EndPoint /Categoria/ID

--- src/main/java/br/com/kriger/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/kriger/cursomc/services/CategoriaService.java
@@ -5,15 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.kriger.cursomc.domain.Categoria;
 import br.com.kriger.cursomc.repositories.CategoriaRepository;
+import br.com.kriger.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
+	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
 	public Categoria find(Integer id) {
-		Optional<?> categoria = categoriaRepository.findById(id);
-		return (Categoria) categoria.orElse(null);
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
